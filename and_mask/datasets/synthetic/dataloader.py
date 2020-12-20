d6d5fd99c7246703cf9d9399a318549134038886
A M <nil.objc@gmail.com>
2020-12-19 19:34:56 -0500

    added and used strog/weak dataset for dataloader

--- and_mask/datasets/synthetic/dataloader.py
+++ and_mask/datasets/synthetic/dataloader.py
@@ -1,7 +1,7 @@
 import torch
 from torch.utils.data import TensorDataset
 from sklearn.datasets import make_classification, make_blobs
-from and_mask.datasets.synthetic.synthetic_data_gen import get_spirals_dataset
+from and_mask.datasets.synthetic.synthetic_data_gen import get_spirals_dataset, get_strong_weak_dataset
 
 def make_dataloader(n_examples, env, n_envs, n_revolutions, n_dims,
                     batch_size,
@@ -10,14 +10,9 @@ def make_dataloader(n_examples, env, n_envs, n_revolutions, n_dims,
                     seed=None):
 
 
-    inputs, labels = make_blobs(n_examples, n_dims + 2, 2, shuffle=False, random_state=seed)
-    # inputs, labels = get_spirals_dataset(n_examples,
-    #                                      n_rotations=n_revolutions,
-    #                                      env=env,
-    #                                      n_envs=n_envs,
-    #                                      n_dims_signatures=n_dims,
-    #                                      seed=seed
-    #                                      )
+    inputs, labels = get_strong_weak_dataset(n_samples=n_examples,glob_sigma=1, sig_sigma=1, K=n_dims+2, rand_strong_coefs=3., inv_weak_coefs=0.3,\
+                                                 n_informative=2, n_redundant=0, use_sklearn=False, batch_size=batch_size, seed=seed)
+
     if flip_first_signature:
         inputs[:1, 2:] = -inputs[:1, 2:]
 
--- and_mask/datasets/synthetic/synthetic_data_gen.py
+++ and_mask/datasets/synthetic/synthetic_data_gen.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.random import default_rng
 
 def get_spirals_dataset(n_examples, n_rotations, env, n_envs,
                         n_dims_signatures,
@@ -38,3 +39,39 @@ def get_spirals_dataset(n_examples, n_rotations, env, n_envs,
     inputs = np.hstack((mechanisms, signatures))
 
     return inputs.astype(np.float32), labels.astype(np.float32)
+
+def get_strong_weak_dataset(n_samples,glob_sigma,sig_sigma,K,rand_strong_coefs,inv_weak_coefs,n_informative,n_redundant,use_sklearn, batch_size, seed):
+    # more info for sklearn dataset generation: https://scikit-learn.org/stable/modules/generated/sklearn.datasets.make_classification.html#sklearn.datasets.make_classification
+    rng = default_rng(seed)
+    X = rng.normal(0, glob_sigma, (n_samples, K))
+    y = rng.choice(2, size=n_samples, replace=True)
+
+    # pick a subset of best predictors at random 
+    true_rng = default_rng(0)
+    true_pred_indices = true_rng.choice(K, size=1, replace=False)
+    rand_best_indices = rng.choice(K, size=1, replace=False)
+
+    # check to make sure these randoms aren't out true pred
+    condition = True
+    while condition:
+        for i,rbp in enumerate(rand_best_indices):
+            if rbp in true_pred_indices:
+                #rand_best_indices[i] = np.random.randint(K, size=1)
+                rand_best_indices[i] = rng.choice(K, size=1, replace=False)
+                condition = True
+                break
+            else:
+                condition = False
+    print(true_pred_indices,rand_best_indices)
+    # set the coefficients at the right row 
+    # that separates each class
+    for n in range(n_samples):
+        if y[n] == 1:
+            X[n,true_pred_indices] = inv_weak_coefs # +  np.random.normal(0,sig_sigma)
+            X[n,rand_best_indices] = rand_strong_coefs + rng.normal(0,sig_sigma)# + dist.sample(X[e,rand_best_indices].shape)  #
+
+        elif y[n] == 0:
+            X[n,true_pred_indices] = 0 #+  np.random.normal(0,sig_sigma) #-inv_weak_coefs 
+            X[n,rand_best_indices] = 0 + rng.normal(0,sig_sigma) # dist.sample(X[e,rand_best_indices].shape)
+
+    return X, y
